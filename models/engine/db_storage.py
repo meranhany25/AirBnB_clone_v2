612b9256da65a2d3d3817cd080148955c0c868ec
loda26 <mokhaled26498@gmail.com>
2024-02-21 00:09:52 +0200

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,8 +3,6 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-import urllib.parse
-
 from models.base_model import BaseModel, Base
 from models.state import State
 from models.city import City
