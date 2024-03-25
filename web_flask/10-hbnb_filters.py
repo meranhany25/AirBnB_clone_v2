1d6de0ec40a07aa1a1dea271824cc7e55cac2c69
meranhan25 <meranhany25@gmail.com>
2024-03-25 02:00:34 +0200

    m

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-start Flask application
-"""
+route  :   /hbnb_filters: display a HTML page like 6-index.html,
+which was done during the project 0x01. AirBnB clone - Web static
 
+"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """display a HTML page like 6-index.html"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html", states=states,
                            amenities=amenities)
 
 
+app.debug = True
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown_context(ctx):
+    """Displays cities per state"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
